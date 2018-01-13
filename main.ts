3c9102c9d33b63f3d6a95f3a5488497311b56b53
DRAgon734465502 <zj734465502@gmail.com>
2018-01-13 02:04:06 -0800

    microbit0112

--- main.ts
+++ main.ts
@@ -83,7 +83,7 @@ namespace robotbit {
 
     //% blockId=robotbit_zj block="zjx|pin1 %pin"
     //% weight=100
-    export function Ultrasonic(pin: DigitalPin): number {
+    export function UM(pin: DigitalPin): number {
 /*
         // send pulse
         pins.setPull(pin, PinPullMode.PullNone);
